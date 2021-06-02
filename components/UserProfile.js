2b57e23421a3c6f7948574e863b854aa2511aa52
nakseono <nakseono@gmail.com>
2021-06-02 18:54:45 +0900

    add UserProfile format

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Card, Avatar, Button } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import styled from "styled-components";
+
+const ButtonStyle = styled(Button)`
+  font-size: 11px;
+  position: absolute;
+  width: 50px;
+  height: 25px;
+  top: 20px;
+  right: 17px;
+  text-align: center;
+  padding: 0;
+  padding-left: 4px;
+  border-radius: 14px;
+`;
+
+const profileDefine = ({ server, mainChar, mainCharLv }) => {
+  return `${server} | ${mainChar} - Lv.${mainCharLv}`;
+};
 
 const UserProfile = () => {
-  return <div>유저 프로필</div>;
+  return (
+    <Card
+      style={{ marginBottom: "10px" }}
+      actions={[
+        <SettingOutlined key="setting" />,
+        <EditOutlined key="edit" />,
+        // <EllipsisOutlined key="ellipsis" />,
+      ]}
+    >
+      <ButtonStyle>로그아웃</ButtonStyle>
+      <Card.Meta
+        avatar={<Avatar>프사</Avatar>}
+        title="유저 닉네임 님"
+        description={profileDefine("test", "test", "test")}
+      />
+    </Card>
+  );
 };
 
 export default UserProfile;
