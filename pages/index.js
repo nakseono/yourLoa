c776298e80f498e31ba3ac356e518cd1744b0a84
nakseono <nakseono@gmail.com>
2021-06-29 15:32:05 +0900

    배너 예시들 추가

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,24 @@ import styled from "styled-components";
 import AppLayout from "../components/AppLayout.js";
 
 const BannerWrapper = styled.div`
-  width: 900px;
-  height: 200px;
+  /* width: 900px; */
+  height: 7em;
   border: solid 1px;
+  margin-bottom: 1em;
 `;
 
 const InLayoutContent = styled.div`
-  padding: 10px;
+  padding: 1em;
 `;
 
 const Main = () => {
   return (
     <AppLayout>
       <InLayoutContent>
-        <BannerWrapper></BannerWrapper>
+        <BannerWrapper>놓치지 말아야 할 이벤트 배너</BannerWrapper>
+        <BannerWrapper>놓치지 말아야 할 이벤트 배너</BannerWrapper>
+        <BannerWrapper>꿀팁 배너</BannerWrapper>
+        <BannerWrapper>꿀팁 배너</BannerWrapper>
       </InLayoutContent>
     </AppLayout>
   );
