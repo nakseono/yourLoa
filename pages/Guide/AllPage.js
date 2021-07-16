9dc8b3457d75bac845e157342765ab523a8d43c5
nakseono <nakseono@gmail.com>
2021-07-16 19:14:43 +0900

    전사 페이지 구조 참고해서 위로 올림

--- pages/Guide/AllPage.js
+++ pages/Guide/AllPage.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { Card } from "antd";
 
 import WarriorPage from "./GuidePages/Warrior/WarriorPage";
 import MagicianPage from "./GuidePages/Magician/MagicianPage";
@@ -7,13 +8,104 @@ import FighterPage from "./GuidePages/Fighter/FighterPage";
 import AssasinPage from "./GuidePages/Assasin/AssasinPage";
 
 const AllPage = () => {
+  const [isWarrior, setWarriorPage] = useState(false);
+  const [isMagician, setMagicianPage] = useState(false);
+  const [isHunter, setHunterPage] = useState(false);
+  const [isFighter, setFighterPage] = useState(false);
+  const [isAssasin, setAssasinPage] = useState(false);
+
+  const clearState = useCallback(() => {
+    setWarriorPage(false);
+    setMagicianPage(false);
+    setHunterPage(false);
+    setFighterPage(false);
+    setAssasinPage(false);
+  }, []);
+
+  const clickWarrior = () => {
+    clearState();
+    setWarriorPage(true);
+  };
+  const clickMagician = () => {
+    clearState();
+    setMagicianPage(true);
+  };
+  const clickHunter = () => {
+    clearState();
+    setHunterPage(true);
+  };
+  const clickFighter = () => {
+    clearState();
+    setFighterPage(true);
+  };
+  const clickAssasin = () => {
+    clearState();
+    setAssasinPage(true);
+  };
+
   return (
     <>
-      <WarriorPage />
-      {/* <MagicianPage />
-      <HunterPage />
-      <FighterPage />
-      <AssasinPage /> */}
+      <Card
+        title="전 클래스"
+        headStyle={{ display: "flex", justifyContent: "center" }}
+        style={{ border: "0.5px solid", marginTop: "2rem" }}
+      >
+        <Card.Grid
+          style={{
+            width: "20%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickWarrior}
+        >
+          <div>전사</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{
+            width: "20%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickMagician}
+        >
+          <div>마법사</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{
+            width: "20%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickHunter}
+        >
+          <div>헌터</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{
+            width: "20%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickFighter}
+        >
+          <div>무도가</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{ width: "20%", justifyContent: "center", display: "flex" }}
+          onClick={clickAssasin}
+        >
+          <div>암살자</div>
+        </Card.Grid>
+      </Card>
+      {isWarrior ? <WarriorPage /> : <></>}
+      {isMagician ? <MagicianPage /> : <></>}
+      {isHunter ? <HunterPage /> : <></>}
+      {isFighter ? <FighterPage /> : <></>}
+      {isAssasin ? <AssasinPage /> : <></>}
     </>
   );
 };
