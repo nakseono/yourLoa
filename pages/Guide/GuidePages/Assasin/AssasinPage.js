140fa782f44d2f78fb7aafc0419dced9bc63db4e
nakseono <nakseono@gmail.com>
2021-07-18 20:35:45 +0900

    암살자 페이지 추가

--- pages/Guide/GuidePages/Assasin/AssasinPage.js
+++ pages/Guide/GuidePages/Assasin/AssasinPage.js
@@ -1,13 +1,83 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Card } from "antd";
+import styled from "styled-components";
+
+import Blade from "./Blade";
+import Demonic from "./Demonic";
+import Reaper from "./Reaper";
 
 const AssasinPage = () => {
+  const [isBlade, setBladePage] = useState(false);
+  const [isDemonic, setDemonicPage] = useState(false);
+  const [isReaper, setReaperPage] = useState(false);
+
+  const clearState = useCallback(() => {
+    setBladePage(false);
+    setDemonicPage(false);
+    setReaperPage(false);
+  }, []);
+
+  const clickBlade = () => {
+    clearState();
+    setBladePage(true);
+  };
+  const clickDemonic = () => {
+    clearState();
+    setDemonicPage(true);
+  };
+  const clickReaper = () => {
+    clearState();
+    setReaperPage(true);
+  };
+
+  const CardWrapper = styled(Card)`
+    border: 0.5px solid;
+    margin-top: 1rem;
+  `;
+
   return (
     <>
-      <Card title="Assasin">
-        <Card.Grid>Content</Card.Grid>
-        <Card.Grid>Content</Card.Grid>
-      </Card>
+      <CardWrapper
+        title="암살자"
+        headStyle={{ display: "flex", justifyContent: "center" }}
+        size="small"
+      >
+        <Card.Grid
+          style={{
+            width: "33.33%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickBlade}
+        >
+          <div>블레이드</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{
+            width: "33.33%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickDemonic}
+        >
+          <div>데모닉</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{
+            width: "33.33%",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickReaper}
+        >
+          <div>리퍼</div>
+        </Card.Grid>
+      </CardWrapper>
+      {isBlade ? <Blade /> : <></>}
+      {isDemonic ? <Demonic /> : <></>}
+      {isReaper ? <Reaper /> : <></>}
     </>
   );
 };
