eff210a71b66e80a838a30ae7750fb8cbddbec57
nakseono <nakseono@gmail.com>
2021-07-18 20:29:00 +0900

    헌터 직업군 추가

--- pages/Guide/GuidePages/Hunter/HunterPage.js
+++ pages/Guide/GuidePages/Hunter/HunterPage.js
@@ -1,13 +1,118 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Card } from "antd";
+import styled from "styled-components";
+
+import Blaster from "./Blaster";
+import DevilHunter from "./DevilHunter";
+import HawkEye from "./HawkEye";
+import Scouter from "./Scouter";
+import Gunslinger from "./Gunslinger";
 
 const HunterPage = () => {
+  const [isBlaster, setBlasterPage] = useState(false);
+  const [isDevilHunter, setDevilHunterPage] = useState(false);
+  const [isHawkEye, setHawkEyePage] = useState(false);
+  const [isScouter, setScouterPage] = useState(false);
+  const [isGunslinger, setGunslingerPage] = useState(false);
+
+  const clearState = useCallback(() => {
+    setBlasterPage(false);
+    setDevilHunterPage(false);
+    setHawkEyePage(false);
+    setScouterPage(false);
+    setGunslingerPage(false);
+  }, []);
+
+  const clickBlaster = () => {
+    clearState();
+    setBlasterPage(true);
+  };
+  const clickDevilHunter = () => {
+    clearState();
+    setDevilHunterPage(true);
+  };
+  const clickHawkEye = () => {
+    clearState();
+    setHawkEyePage(true);
+  };
+  const clickScouter = () => {
+    clearState();
+    setScouterPage(true);
+  };
+  const clickGunslinger = () => {
+    clearState();
+    setGunslingerPage(true);
+  };
+
+  const CardWrapper = styled(Card)`
+    border: 0.5px solid;
+    margin-top: 1rem;
+  `;
+
   return (
     <>
-      <Card title="Hunter">
-        <Card.Grid>Content</Card.Grid>
-        <Card.Grid>Content</Card.Grid>
-      </Card>
+      <CardWrapper
+        title="헌터"
+        headStyle={{ display: "flex", justifyContent: "center" }}
+        size="small"
+      >
+        <Card.Grid
+          style={{
+            width: "20%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickBlaster}
+        >
+          <div>블래스터</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{
+            width: "20%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickDevilHunter}
+        >
+          <div>데빌헌터</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{
+            width: "20%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickHawkEye}
+        >
+          <div>호크아이</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{
+            width: "20%",
+            borderRight: "solid 0.5px",
+            justifyContent: "center",
+            display: "flex",
+          }}
+          onClick={clickScouter}
+        >
+          <div>스카우터</div>
+        </Card.Grid>
+        <Card.Grid
+          style={{ width: "20%", justifyContent: "center", display: "flex" }}
+          onClick={clickGunslinger}
+        >
+          <div>건슬링어</div>
+        </Card.Grid>
+      </CardWrapper>
+
+      {isBlaster ? <Blaster /> : <></>}
+      {isDevilHunter ? <DevilHunter /> : <></>}
+      {isHawkEye ? <HawkEye /> : <></>}
+      {isScouter ? <Scouter /> : <></>}
+      {isGunslinger ? <Gunslinger /> : <></>}
     </>
   );
 };
