c55964a990ba7f59341927a347cf268b16a3807f
nakseono <nakseono@gmail.com>
2021-07-22 21:43:09 +0900

    각 직업 표기 기초 틀 잡음

--- pages/Guide/GuidePages/Hunter/Scouter.js
+++ pages/Guide/GuidePages/Hunter/Scouter.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Descriptions } from "antd";
 
 const Scouter = () => {
-  return <div>스카우터 페이지</div>;
+  return (
+    <>
+      <Descriptions title="스카우터" bordered>
+        <Descriptions.Item label="군단장 장비">6지배</Descriptions.Item>
+        <Descriptions.Item label="메인 특성">특화</Descriptions.Item>
+        <Descriptions.Item label="보조 특성">치명</Descriptions.Item>
+
+        <Descriptions.Item label="각인" span={3}>
+          원한 / 돌격대장 / 바리게이트 / 각성 or 기습의 대가 or 예리한 둔기 /
+          진화의 유산
+        </Descriptions.Item>
+      </Descriptions>
+    </>
+  );
 };
 
 export default Scouter;
