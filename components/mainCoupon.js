2ca61784313bd10b6948f531971073511141b653
nakseono <nakseono@gmail.com>
2021-08-03 17:59:27 +0900

    쿠폰 copy 기능 추가 및 css 추가

--- components/mainCoupon.js
+++ components/mainCoupon.js
@@ -1,11 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import { Typography } from "antd";
+
+const { Paragraph } = Typography;
+
+const TextCopy = styled(Paragraph)`
+  margin-bottom: 0 !important;
+  font-size: 19px;
+  font-weight: bold;
+  color: #d3687f;
+`;
 
 const CouponWrapper = styled.div`
-  height: 5rem;
+  height: 6rem;
   border: solid 1px;
   width: 30%;
-  margin-bottom: 1em;
   display: inline-flex;
   justify-content: center;
   align-items: center;
@@ -18,18 +27,12 @@ const CouponTitle = styled.div`
   font-weight: 700;
 `;
 
-const CouponCode = styled.div`
-  font-size: 19px;
-  font-weight: bold;
-  color: #d3687f;
-`;
-
 const Coupon = ({ title, date, code }) => {
   return (
     <CouponWrapper>
       <CouponTitle>{title}</CouponTitle>
       <div>{date}</div>
-      <CouponCode>{code}</CouponCode>
+      <TextCopy copyable>{code}</TextCopy>
     </CouponWrapper>
   );
 };
