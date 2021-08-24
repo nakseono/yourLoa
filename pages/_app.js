3c5fb91dc09355d9d2853fc92539a536dbf28155
nakseono <nakseono@gmail.com>
2021-08-24 22:28:03 +0900

    CookiesProvider 감싸기

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import { CookiesProvider } from "react-cookie";
 
 import "./App.css";
 
-const ComponentWrapper = styled.div`
+const ComponentWrapper = styled(CookiesProvider)`
   width: 1400px;
   padding: 20px 30px;
   margin: auto;
