bbdcf99900c0aad6a72ba1b23721fc29eef7420e
nakseono <nakseono@gmail.com>
2021-09-07 21:55:16 +0900

    유용한 링크 추가

--- pages/Mokoko.js
+++ pages/Mokoko.js
@@ -9,6 +9,7 @@ const Mokoko = () => {
       <div>오르페우스의 별 획득처 & 공략</div>
       <div>거인의 심장 획득처 & 공략</div>
       <div>etc ...</div>
+      {/* https://gall.dcinside.com/board/view/?id=lostark&no=3571561 이 게시글 참고하면 좋을 듯함. */}
     </AppLayout>
   );
 };
