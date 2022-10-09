35f233e3b05dccbd934bc087fb255f610b8812e2
nakseono <nakseono@gmail.com>
2022-10-10 01:54:45 +0900

    add layout

---
+++ pages/auction.js
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import AppLayout from "../components/AppLayout";
+
+const Auction = () => {
+  return (
+    <AppLayout>
+      auction.js
+    </AppLayout>
+  );
+};
+
+export default Auction;
