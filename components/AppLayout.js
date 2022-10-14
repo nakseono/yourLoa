f2d7d729c4347ef2f1451ba72cf168dc1d920a5a
nakseono <nakseono@gmail.com>
2022-10-14 14:31:01 +0900

    antd menu update

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -17,72 +17,88 @@ const Logo = styled.div`
   background: rgba(255, 255, 255, 0.2);
 `;
 
-const AppLayout = ({ children }) => (
-  <Layout>
-    <Header
-      style={{
-        position: "fixed",
-        zIndex: 1,
-        width: "100%",
-      }}
-    >
-      <Link href="/">
-        <a>
-          <Logo />
-        </a>
-      </Link>
-      <Menu theme="dark" mode="horizontal">
-        <Menu.Item key="first">
-          <Link href="/">
-            <a>캐릭터 검색</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="second">
-          <Link href="/squadMaker">
-            <a>스쿼드 메이커</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="third">
-          <Link href="/auction">
-            <a>경매 분배금 계산기</a>
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="four">
-          <Link href="/board">
-            <a>자유게시판</a>
-          </Link>
-        </Menu.Item>
-      </Menu>
-    </Header>
-    <Content
-      className="site-layout"
-      style={{
-        padding: "20px 50px",
-        marginTop: 64,
-        minHeight: 600,
-      }}
-    >
-      {children}
-    </Content>
-    <Footer
-      style={{
-        textAlign: "center",
-      }}
-    >
-      <div>
-        YourLoa ©made by{" "}
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/nakseono"
-        >
-          nakseono
-        </a>
-        <br />
-        contact : nakseono@gmail.com
-      </div>
-    </Footer>
-  </Layout>
-);
+const AppLayout = ({ children }) => {
+  const items = [
+    {
+      label: (
+        <Link href="/">
+          <a>캐릭터 검색</a>
+        </Link>
+      ),
+      key: "home",
+    },
+    {
+      label: (
+        <Link href="/squadMaker">
+          <a>스쿼드 메이커</a>
+        </Link>
+      ),
+      key: "squadMaker",
+    },
+    {
+      label: (
+        <Link href="/auction">
+          <a>경매 분배금 계산기</a>
+        </Link>
+      ),
+      key: "auction",
+    },
+    {
+      label: (
+        <Link href="/board">
+          <a>자유게시판</a>
+        </Link>
+      ),
+      key: "board",
+    },
+  ];
+
+  return (
+    <Layout>
+      <Header
+        style={{
+          position: "fixed",
+          zIndex: 1,
+          width: "100%",
+        }}
+      >
+        <Link href="/">
+          <a>
+            <Logo />
+          </a>
+        </Link>
+        <Menu theme="dark" mode="horizontal" items={items} />
+      </Header>
+      <Content
+        className="site-layout"
+        style={{
+          padding: "35px 100px",
+          marginTop: 64,
+          minHeight: 800,
+        }}
+      >
+        {children}
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <div>
+          YourLoa ©made by{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/nakseono"
+          >
+            nakseono
+          </a>
+          <br />
+          contact : nakseono@gmail.com
+        </div>
+      </Footer>
+    </Layout>
+  );
+};
 
 export default AppLayout;
